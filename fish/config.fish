70ee4e0b06867fbf6cc28a8d7ca86027121bd8bd
m-88888888 <mochi.h8888@gmail.com>
2020-12-27 23:27:39 +0900

    config.fish追加

--- fish/config.fish
+++ fish/config.fish
@@ -34,3 +34,9 @@ set -g theme_display_git_master_branch yes # git branch名を表示
 set -g theme_display_date no  # 時刻を表示しないように設定
 set -g theme_display_cmd_duration no  # コマンド実行時間の非表示
 
+# cdしたらls
+functions --copy cd standard_cd
+
+function cd
+  standard_cd $argv; and ls
+end
