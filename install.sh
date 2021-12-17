c418e035535c4da862fd471c47d806a56162da25
m-88888888 <mochi.h8888@gmail.com>
2021-12-18 00:30:55 +0900

    Update

--- install.sh
+++ install.sh
@@ -22,58 +22,58 @@ echo "Finish creating symlink."
 echo ""
 
 # homebrew install
-echo "Start the installation of homebrew."
-if [ "$(uname)" == 'Linux' ]; then
-
-  brew help &> /dev/null 2>&1
-  if [ $? -eq 127 ]; then
-    echo "installing homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-    echo "run brew doctor..."
-    which brew >/dev/null 2>&1 && brew doctor
-
-    echo "run brew update..."
-    which brew >/dev/null 2>&1 && brew update
-
-    echo "ok. run brew upgrade..."
-    brew upgrade
-
-    if [ -e "Brewfile" ]; then
-      brew bundle
-    fi
-  else
-    echo "homebrew is already installed."
-  fi
-
-elif [ "$(uname)" == 'Darwin' ]; then
-
-  brew help &> /dev/null 2>&1
-  if [ $? -eq 127 ]; then
-    echo "installing homebrew..."
-    which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-    echo "run brew doctor..."
-    which brew >/dev/null 2>&1 && brew doctor
-
-    echo "run brew update..."
-    which brew >/dev/null 2>&1 && brew update
-
-    echo "ok. run brew upgrade..."
-    brew upgrade
-
-    if [ -e "Brewfile" ]; then
-      brew bundle
-    fi
-  fi
-
-else
-  echo "Your platform ($(uname -a)) is not supported."
-  exit 1
-fi
-
-echo "Finish the installation of homebrew."
-echo ""
+#echo "Start the installation of homebrew."
+#if [ "$(uname)" == 'Linux' ]; then
+#
+#  brew help &> /dev/null 2>&1
+#  if [ $? -eq 127 ]; then
+#    echo "installing homebrew..."
+#    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#
+#    echo "run brew doctor..."
+#    which brew >/dev/null 2>&1 && brew doctor
+#
+#    echo "run brew update..."
+#    which brew >/dev/null 2>&1 && brew update
+#
+#    echo "ok. run brew upgrade..."
+#    brew upgrade
+#
+#    if [ -e "Brewfile" ]; then
+#      brew bundle
+#    fi
+#  else
+#    echo "homebrew is already installed."
+#  fi
+#
+#elif [ "$(uname)" == 'Darwin' ]; then
+#
+#  brew help &> /dev/null 2>&1
+#  if [ $? -eq 127 ]; then
+#    echo "installing homebrew..."
+#    which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#
+#    echo "run brew doctor..."
+#    which brew >/dev/null 2>&1 && brew doctor
+#
+#    echo "run brew update..."
+#    which brew >/dev/null 2>&1 && brew update
+#
+#    echo "ok. run brew upgrade..."
+#    brew upgrade
+#
+#    if [ -e "Brewfile" ]; then
+#      brew bundle
+#    fi
+#  fi
+#
+#else
+#  echo "Your platform ($(uname -a)) is not supported."
+#  exit 1
+#fi
+#
+#echo "Finish the installation of homebrew."
+#echo ""
 
 
 # TODO git clone tpm (tmux plugin)
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,7 +1,7 @@
 # zsh theme
 zinit ice depth=1; zinit light romkatv/powerlevel10k
 # syntax hightlight
-zinit light zdharma/fast-syntax-highlighting
+zinit light zdharma-continuum/fast-syntax-highlighting
 # autosuggest
 zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
