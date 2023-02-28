5da29ee140833ab5c3b0edaba73489596e93c406
m-88888888 <mochi.h8888@gmail.com>
2023-02-28 09:39:30 +0900

    add git search keymap of telescope.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -393,6 +393,8 @@ require('telescope').setup{
 -- require('telescope').load_extension('fzf')
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fp', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
