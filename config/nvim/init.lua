0ca86fae1f858c7f44756a864cf3261eee95ce39
m-88888888 <mochi.h8888@gmail.com>
2023-09-20 02:01:12 +0900

    update gitsigns config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -272,17 +272,23 @@ end
 
 require('gitsigns').setup {
   signs                        = {
-    add          = { text = '│' },
-    change       = { text = '│' },
-    delete       = { text = '_' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
+    -- add          = { text = '│' },
+    -- change       = { text = '│' },
+    -- delete       = { text = '_' },
+    -- topdelete    = { text = '‾' },
+    -- changedelete = { text = '~' },
+    -- untracked    = { text = '┆' },
+    add = { hl = 'GitSignsAdd', text = ' ▎', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+    change = { hl = 'GitSignsChange', text = ' ▎', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
+    delete = { hl = 'GitSignsDelete', text = ' ', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    topdelete = { hl = 'GitSignsDelete', text = ' ', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
+    changedelete = { hl = 'GitSignsChange', text = '▎ ', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
   },
   signcolumn                   = true, -- Toggle with `:Gitsigns toggle_signs`
   numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
   linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
   word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+
   watch_gitdir                 = {
     follow_files = true
   },
