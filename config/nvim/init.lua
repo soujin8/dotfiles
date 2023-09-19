4e90c3eeea6f65d7ae3a893d15f829d1d20fac47
m-88888888 <mochi.h8888@gmail.com>
2023-09-20 01:40:00 +0900

    add copy relative file path cmd

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -569,3 +569,17 @@ vim.keymap.set('n', '<leader>gc', ":Neogit commit -v<CR>", {})
 
 -- yamlやmarkdown,gitcommitのときにもcopilotを有効にする
 vim.g.copilot_filetypes = { markdown = true, gitcommit = true, yaml = true }
+
+function copy_relative_file_path_to_clipboard()
+  local file_path = vim.fn.expand("%:~:.")
+  if vim.fn.has('clipboard') == 1 then
+    vim.fn.setreg('+', file_path)
+    print("Copied relative file path to clipboard: " .. file_path)
+  else
+    print("Clipboard support is not available")
+  end
+end
+
+-- キーバインドの設定 (例: <Leader>c で呼び出す)
+vim.api.nvim_set_keymap('n', '<Leader>n', [[<Cmd>lua copy_relative_file_path_to_clipboard()<CR>]],
+  { noremap = true, silent = true })
