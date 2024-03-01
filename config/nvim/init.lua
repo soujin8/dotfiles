ce344b32fddcbe936522525d78cfbe2fb404774f
m-88888888 <mochi.h8888@gmail.com>
2024-03-01 11:46:17 +0900

    remove

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -9,9 +9,3 @@ require('rc.keymaps')
 require('rc.plugin_manager')
 require('rc.options')
 require('rc.functions')
-
----- fern.vim
----- vim.keymap.set("n", "<Leader>df", ":Fern . -drawer<CR>")
----- vim.g['fern#default_hidden'] = 1
----- vim.g['fern#renderer'] = 'nerdfont'
----- vim.g['fern#renderer#nerdfont#indent_markers'] = 1
