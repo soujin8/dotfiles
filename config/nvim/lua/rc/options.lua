593f3224ed1e1b601232c0e19b1f7d8d14fa406b
m-88888888 <mochi.h8888@gmail.com>
2024-04-20 17:21:44 +0900

    disable python3 provider

--- config/nvim/lua/rc/options.lua
+++ config/nvim/lua/rc/options.lua
@@ -71,3 +71,4 @@ vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? '<C-y>' : '<CR>'", { expr =
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
