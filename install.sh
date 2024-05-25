84c6e83c239298b9c1177f18fa174e9f08a270c3
soujin8 <mochi.h8888@gmail.com>
2024-05-25 16:05:56 +0900

    change github username

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-INSTALL_DIR="${INSTALL_DIR:-$HOME/dev/github.com/m-88888888/dotfiles}"
+INSTALL_DIR="${INSTALL_DIR:-$HOME/dev/github.com/soujin8/dotfiles}"
 
 if [ -d "$INSTALL_DIR" ]; then
 	echo "Updating dotfiles..."
 	git -C "$INSTALL_DIR" pull
 else
 	echo "Installing dotfiles..."
-	git clone https://github.com/m-88888888/dotfiles.git "$INSTALL_DIR"
+	git clone https://github.com/soujin8/dotfiles.git "$INSTALL_DIR"
 fi
 
 /bin/bash "$INSTALL_DIR/scripts/setup.bash"
