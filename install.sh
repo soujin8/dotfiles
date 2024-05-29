9da40c61000eddb76533bb09b4e81907eead71ce
soujin8 <mochi.h8888@gmail.com>
2024-05-29 23:46:44 +0900

    update

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 INSTALL_DIR="${INSTALL_DIR:-$HOME/dev/github.com/soujin8/dotfiles}"
 
 if [ -d "$INSTALL_DIR" ]; then
-	echo "Updating dotfiles..."
-	git -C "$INSTALL_DIR" pull
+	echo "Already cloned."
 else
 	echo "Installing dotfiles..."
 	git clone https://github.com/soujin8/dotfiles.git "$INSTALL_DIR"
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -15,5 +15,7 @@ mkdir -p "$XDG_CONFIG_HOME"
 ln -sfv "$REPO_DIR/config/"* "$XDG_CONFIG_HOME"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
 ln -sfv "$XDG_CONFIG_HOME/tmux/tmux.conf" "$HOME/.tmux.conf"
+ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.i3" $HOME
+ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.Xmodemap" $HOME
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
