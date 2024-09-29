9dca54c46f8fd46539e1fe544db7d7957efda00d
soujin8 <mochi.h8888@gmail.com>
2024-09-29 14:58:58 +0900

    ブロックコメント対応

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -6,6 +6,11 @@ return {
       -- add any options here
     },
     lazy = false,
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end
   },
   {
     "windwp/nvim-autopairs",
