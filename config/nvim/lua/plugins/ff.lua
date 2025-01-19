a8422f9a3978bd4daf2325126220dc9414c690ad
soujin8 <mochi.h8888@gmail.com>
2025-01-19 12:21:35 +0900

    update

--- config/nvim/lua/plugins/ff.lua
+++ config/nvim/lua/plugins/ff.lua
@@ -1,8 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.6',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-fzf-native.nvim' },
     config = function()
       require('telescope').setup {
         defaults = {
@@ -18,7 +17,7 @@ return {
       }
 
       require('telescope').load_extension('fzf')
-      -- require('telescope').load_extension('file_browser')
+
       local builtin = require('telescope.builtin')
 
       -- when a count N is given to a telescope mapping called through the following
@@ -28,25 +27,19 @@ return {
       end
 
       local map, opts = vim.keymap.set, { noremap = true, silent = true }
-      -- map("n", "<leader>ff", function() telescope_cwd('find_files', { hidden = true }) end, opts)
-      map("n", "<leader>ff", function() telescope_cwd('git_files', { hidden = true }) end, opts)
+      map("n", "<leader>ff", function() telescope_cwd('find_files', { hidden = true }) end, opts)
+      map("n", "<leader>gf", function() telescope_cwd('git_files', { hidden = true }) end, opts)
       map("n", "<leader>gr", function() telescope_cwd('live_grep') end, opts)
       map("n", "<leader>ds", builtin.lsp_document_symbols, opts)
       map("n", "<leader>ws", builtin.lsp_dynamic_workspace_symbols, opts)
       map('n', '<leader>fb', builtin.buffers, opts)
       map("n", "<leader>fo", builtin.oldfiles, opts)
       map("n", "<leader>ts", "<cmd>Telescope<cr>", opts)
-      -- map("n", "<leader>df", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", opts)
     end
   },
   {
     'nvim-telescope/telescope-fzf-native.nvim',
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    build =
-    'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    dependencies = { "nvim-lua/plenary.nvim" },
+    build ='cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
   },
-  -- {
-  --   "nvim-telescope/telescope-file-browser.nvim",
-  --   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-  -- },
 }
