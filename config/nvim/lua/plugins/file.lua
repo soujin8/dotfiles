8f88e885ec0417cd2ce6176c667d3ce638c9088b
soujin8 <mochi.h8888@gmail.com>
2025-03-13 22:27:58 +0900

    add keymap

--- config/nvim/lua/plugins/file.lua
+++ config/nvim/lua/plugins/file.lua
@@ -19,5 +19,9 @@ return {
     }
 
     vim.keymap.set('n', 'fd', '<cmd>NvimTreeOpen<CR>', { noremap = true, silent = true })
+
+    vim.keymap.set("n", "<leader>tf", function()
+      require("nvim-tree.api").tree.find_file({ open = true, focus = true })
+    end, { desc = "NvimTree: Open & Focus Current File" })
   end
 }
