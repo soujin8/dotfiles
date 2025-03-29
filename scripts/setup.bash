eb77536e33e70e49a70711618e5f25d2d0257a0a
soujin8 <mochi.h8888@gmail.com>
2025-03-29 12:00:52 +0900

    update

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -15,7 +15,15 @@ mkdir -p "$XDG_CONFIG_HOME"
 ln -sfv "$REPO_DIR/config/"* "$XDG_CONFIG_HOME"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
 ln -sfv "$XDG_CONFIG_HOME/tmux/tmux.conf" "$HOME/.tmux.conf"
-ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.i3" $HOME
-ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.Xmodemap" $HOME
+#ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.i3" $HOME
+#ln -svf "$HOME/dev/github.com/soujin8/dotfiles/.Xmodemap" $HOME
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+if command -v mise >/dev/null 2>&1; then
+  mise install
+  exit 0
+else
+  echo "Error: failed to execute 'mise'"
+  exit 1
+fi
