a2f1f2b90af7a679144340bb2f2125460f9df8b1
soujin8 <mochi.h8888@gmail.com>
2025-11-01 17:36:51 +0900

    fix homebrew path settings

--- macos/install-brew.sh
+++ macos/install-brew.sh
@@ -18,7 +18,7 @@ if ! type brew &> /dev/null ; then
 
   # PATHを通す
   if [[ -f "/opt/homebrew/bin/brew" ]]; then
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+    export PATH=/opt/homebrew/bin:$PATH
     echo "✓ Homebrew PATH configured (Apple Silicon)."
   fi
 else
